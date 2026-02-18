29120f69937062ca0e591f6d66b6c381f030e9fd
Lucass_Alvess <lucas_oliveira-01@outlook.com>
2026-02-18 18:02:05 -0300

    backend

---
+++ backend/demo/src/test/java/com/lucasdev/demo/MedicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
