d6764c84fe1e078215ca603bb00e14162c5ff9ee
Lucass_Alvess <lucas_oliveira-01@outlook.com>
2026-02-25 18:15:58 -0300

    CustomError, FieldMessage, ValidationError

---
+++ backend/medication-controller/src/main/java/com/lucasdev/medication/dto/FieldMessage.java
@@ -0,0 +1,20 @@
+package com.lucasdev.medication.dto;
+
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+
+    public FieldMessage(String fieldMessage, String message) {
+        this.fieldName= fieldMessage;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
