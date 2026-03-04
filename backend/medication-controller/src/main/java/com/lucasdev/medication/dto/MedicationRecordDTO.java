25b211596d0e090d22415197112895def1419f9d
Lucass_Alvess <lucas_oliveira-01@outlook.com>
2026-03-04 18:37:01 -0300

    MedicationRecordDTO and MedicineDTO

---
+++ backend/medication-controller/src/main/java/com/lucasdev/medication/dto/MedicationRecordDTO.java
@@ -0,0 +1,63 @@
+package com.lucasdev.medication.dto;
+
+import com.lucasdev.medication.entities.MedicationRecord;
+
+import java.time.Instant;
+
+public class MedicationRecordDTO {
+
+    private Long id;
+    private Instant prescribedSchedule;
+    private Instant timeTaken;
+    private String status;
+
+    public MedicationRecordDTO() {
+    }
+
+    public MedicationRecordDTO(Long id, Instant prescribedSchedule, Instant timeTaken, String status) {
+        this.id = id;
+        this.prescribedSchedule = prescribedSchedule;
+        this.timeTaken = timeTaken;
+        this.status = status;
+    }
+
+    public MedicationRecordDTO(MedicationRecord entity) {
+        id = entity.getId();
+        prescribedSchedule = entity.getPrescribedSchedule();
+        timeTaken = entity.getTimeTaken();
+        status = entity.getStatus();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getPrescribedSchedule() {
+        return prescribedSchedule;
+    }
+
+    public void setPrescribedSchedule(Instant prescribedSchedule) {
+        this.prescribedSchedule = prescribedSchedule;
+    }
+
+    public Instant getTimeTaken() {
+        return timeTaken;
+    }
+
+    public void setTimeTaken(Instant timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
---
+++ backend/medication-controller/src/main/java/com/lucasdev/medication/dto/MedicineDTO.java
@@ -0,0 +1,104 @@
+package com.lucasdev.medication.dto;
+
+import com.lucasdev.medication.entities.Medicine;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+
+public class MedicineDTO {
+
+    private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisar ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
+    private String name;
+    @NotBlank(message = "Campo requerido")
+    private String dosage;
+    @NotBlank(message = "Campo requerido")
+    private Integer frequency;
+    private Integer stock;
+    @NotBlank(message = "Campo requerido")
+    private Instant date;
+    @NotBlank(message = "Campo requerido")
+    private String applicationType;
+
+    public MedicineDTO() {
+    }
+
+    public MedicineDTO(Long id, String name, String dosage, Integer frequency, Integer stock, Instant date, String applicationType) {
+        this.id = id;
+        this.name = name;
+        this.dosage = dosage;
+        this.frequency = frequency;
+        this.stock = stock;
+        this.date = date;
+        this.applicationType = applicationType;
+    }
+
+    public MedicineDTO(Medicine entity) {
+        id = entity.getId();
+        name = entity.getName();
+        dosage = entity.getDosage();
+        frequency = getFrequency();
+        stock = entity.getStock();
+        date = entity.getDate();
+        applicationType = entity.getApplicationType();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public Integer getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Integer frequency) {
+        this.frequency = frequency;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Instant getDate() {
+        return date;
+    }
+
+    public void setDate(Instant date) {
+        this.date = date;
+    }
+
+    public String getApplicationType() {
+        return applicationType;
+    }
+
+    public void setApplicationType(String applicationType) {
+        this.applicationType = applicationType;
+    }
+}
